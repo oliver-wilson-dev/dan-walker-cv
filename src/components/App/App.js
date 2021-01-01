c5b18d1a2b75e67bd8f9927f1f23d97aad4fdbfe
Oliver Wilson <contact.oliver.wilson@gmail.com>
2021-01-01 11:29:07 +0000

    feat: add eslint script

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,21 @@
+import React from 'react';
 import logo from './logo.svg';
 import styles from './App.module.css';
 
 function App() {
   return (
-    <div className={styles["App"]}>
-      <header className={styles["App-header"]}>
-        <img src={logo} className={[styles["App-logo"]]} alt="logo" />
+    <div className={styles.App}>
+      <header className={styles['App-header']}>
+        <img src={logo} className={[styles['App-logo']]} alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          {' '}
+          <code>src/App.js</code>
+          {' '}
+          and save to reload.
         </p>
         <a
-          className={styles["App-link"]}
+          className={styles['App-link']}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
