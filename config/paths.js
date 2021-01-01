67819a887a7b42b617be47ebd0ddb44cbfede7e6
Oliver Wilson <contact.oliver.wilson@gmail.com>
2021-01-01 12:05:03 +0000

    refactor: rename default index to clientEntry for clarity

--- config/paths.js
+++ config/paths.js
@@ -51,7 +51,7 @@ module.exports = {
   appBuild: resolveApp('build'),
   appPublic: resolveApp('public'),
   appHtml: resolveApp('public/index.html'),
-  appIndexJs: resolveModule(resolveApp, 'src/index'),
+  appIndexJs: resolveModule(resolveApp, 'src/clientEntry'),
   appPackageJson: resolveApp('package.json'),
   appSrc: resolveApp('src'),
   appTsConfig: resolveApp('tsconfig.json'),
